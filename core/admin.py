25195b8a5322a1b72df9540e2aad2d1add47c426
Dcampos91 <74212817+Dcampos91@users.noreply.github.com>
2021-04-22 23:19:36 -0400

    inicio de la pagina web

---
+++ core/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Bodega, Cliente, Cocina, Comanda, Departamento, Empleado,\
+     EstadoHabitacion, Factura, HostalClarita, Huesped, OrdenCompra, Pedido, Producto ,Proveedor, TipoHabitacion, Minuta, Usuario, TipoUsuario
+# Register your models here para hacer un CRUD en el area administrador.
+
+admin.site.register(Bodega)
+admin.site.register(Cliente)
+admin.site.register(Cocina)
+admin.site.register(Comanda)
+admin.site.register(Departamento)
+admin.site.register(Empleado)
+admin.site.register(EstadoHabitacion)
+admin.site.register(Factura)
+admin.site.register(HostalClarita)
+admin.site.register(Huesped)
+admin.site.register(Minuta)
+admin.site.register(OrdenCompra)
+admin.site.register(Pedido)
+admin.site.register(Producto)
+admin.site.register(Proveedor)
+admin.site.register(TipoHabitacion)
+admin.site.register(TipoUsuario)
+admin.site.register(Usuario)
---
+++ core/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.db import connection #traera la conexion de oracle y los procesos almacenados
+import cx_Oracle #libreria de oracle
+
+# Create your views here.
+
+def usuarios(request):
+    #data sirve para pasar datos
+    data = {
+        'usuarios':listado_usuarios(),
+        'tipousuario':listado_tipo(),
+    }
+    #guarda los usuarios
+    if request.method == 'POST':
+        nom_usuario = request.POST.get('nombre')
+        clave = request.POST.get('contraseña')
+        tipo_usuario = request.POST.get('tipo')
+        salida = agregar_usuario(nom_usuario, clave, tipo_usuario)
+        if salida == 1:
+            data['mensaje'] = 'agregado correctamente'
+            data['usuarios'] = listado_usuarios()
+        else:
+            data['mensaje'] = 'no se ha podido guardar'
+
+    return render(request, 'core/usuarios.html', data)
+
+# Crear metodo para el listado
+
+def listado_usuarios():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor() # cursor que permite llamar al proceso directamente
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_USUARIOS",[out_cur])
+
+    lista = []#lista para recorrer el cursor
+    for fila in out_cur:
+        lista.append(fila)
+    
+    return lista
+
+def listado_tipo():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor() # cursor que permite llamar al proceso directamente
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_TIPOUSUARIO",[out_cur])
+
+    lista = []#lista para recorrer el cursor
+    for fila in out_cur:
+        lista.append(fila)
+    
+    return lista
+
+def agregar_usuario(nom_usuario, clave, tipo_usuario):
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor() # cursor que permite llamar al proceso directamente
+    salida = cursor.var(cx_Oracle.NUMBER)
+    cursor.callproc('SP_AGREGAR_USUARIO',[nom_usuario, clave, tipo_usuario, salida])
+    return salida.getvalue()
+
